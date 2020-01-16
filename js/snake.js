e04c9a561c7cc5ea93fe6e16d098c6a64e89e3bc
Filip Pie <filipiusz@gmail.com>
2020-01-16 17:55:04 +0100

    food appears at random position, snake gets longer after feeding:)

--- js/snake.js
+++ js/snake.js
@@ -4,6 +4,18 @@ function Snake() {
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   // this function takes in x,y parameters returned from ARROW keyPressed() function
   this.dir = function(x, y) {
@@ -11,28 +23,30 @@ function Snake() {
     this.yspeed = y;
   }
 
-  this.eat = function(pos) {
-    var d = dist(this.x, this.y, pos.x, pos.y);
-    if (d < 1) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
   // snake position refreshing function
   this.update = function() {
+    for (var i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
     this.x = this.x + this.xspeed * scl;
     this.y = this.y + this.yspeed * scl;
 
     // this will keep the snake inside the canvas element
     this.x = constrain(this.x, 0, width - scl);
     this.y = constrain(this.y, 0, height - scl);
+
   }
 
   // this function will draw the snake a noveau each time it's position is recalculated
   this.show = function() {
     fill(255);
+    for (var i = 0; i < this.total; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+
     rect(this.x, this.y, scl, scl);
 
   }
