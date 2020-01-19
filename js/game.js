51b29e380f2e0de14e05b5e4e2f7fb790085d7f3
Filip Pie <filipiusz@gmail.com>
2020-01-19 10:42:43 +0100

    improved snake steering

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ var food;
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
-  frameRate(10);
+  frameRate(8);
   pickLocation();
 }
 
@@ -32,15 +32,15 @@ function draw() {
   rect(food.x, food.y, scl, scl);
 }
 
-// detects input from user
+// detects input from user and prevents backward movement of the snake
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
+  if (keyCode === UP_ARROW && (s.xspeed !== 0 && s.yspeed !== 1)) {
     s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
+  } else if (keyCode === DOWN_ARROW && (s.xspeed !== 0 && s.yspeed !== -1)) {
     s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
+  } else if (keyCode === RIGHT_ARROW && (s.xspeed !== -1 && s.yspeed !== 0)) {
     s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
+  } else if (keyCode === LEFT_ARROW && (s.xspeed !== 1 && s.yspeed !== 0)) {
     s.dir(-1, 0);
   }
 
