f31d1f8f5b559494b2de5b27e86dc1bae1c2b723
Filip Pie <filipiusz@gmail.com>
2020-01-19 11:35:51 +0100

    food spawning on the snake bug fixed

--- js/game.js
+++ js/game.js
@@ -7,15 +7,27 @@ var food;
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
-  frameRate(8);
+  frameRate(8); // game speed
   pickLocation();
 }
 
+// spawns new food for snake to eat
 function pickLocation() {
   var cols = floor(width / scl);
   var rows = floor(height / scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
   food.mult(scl);
+  // preventing new food from appearing on the snake
+  for (var i = 0; i < s.tail.length; i++) {
+    var pos = s.tail[i];
+    var d1 = dist(food.x, food.y, pos.x, pos.y); // snake's tail position
+    var d2 = dist(food.x, food.y, s.x, s.y); // snake's head position
+    if (d1 < 1 || d2 < 1) {
+      console.log('food spawned on snake, restarting food');
+      food = 0;
+      pickLocation();
+    }
+  }
 }
 // in p5.js this function is called in loop
 function draw() {
@@ -43,5 +55,8 @@ function keyPressed() {
   } else if (keyCode === LEFT_ARROW && (s.xspeed !== 1 && s.yspeed !== 0)) {
     s.dir(-1, 0);
   }
-
 }
+
+// function mouseClicked() {
+//   pickLocation();
+// }
